c88687876d682fe5dc6d17898f4be4e501bdb801
George Tao <georgetao258@gmail.com>
2020-12-17 17:58:37 -0800

    changed variable names and cleaned up file directories

--- opponents.py
+++ opponents.py
@@ -1,5 +1,6 @@
 import pytesseract
 import cv2
+import sys
 
 from PIL import Image
 
@@ -9,48 +10,68 @@ import keyboard
 from difflib import SequenceMatcher
 
 pytesseract.pytesseract.tesseract_cmd ='C:/Program Files (x86)/Tesseract-OCR/tesseract.exe'
-game = True
+
+args = sys.argv
+
+
 loadingBind = '='
 keyBind = '`'
+
+game = True
 playersLoaded = True
 count = 0
 count1 = 0
+
+
 while game:
 	#LOADING SCREEN
 	if keyboard.is_pressed(loadingBind) and playersLoaded:
-		# l_img = pyautogui.screenshot('ltest.png')
-		l_img = Image.open('ltest.png')
+		# l_img = pyautogui.screenshot('./screenshots/loadingScreen.png')
+		l_img = Image.open('./screenshots/loadingScreen.png')
 
 		width, height = l_img.size
 		players = []
-		for j in (0, 540):
-			for i in range(4):
+		for y in (0, 540):
+			for x in range(4):
 				#CROPPING IMAGE
-				p_left = (width/2) - 550 + (288 * i)
-				p_top = 485 + j
-				p_right = (width/2) - 310 + (288 * i)
-				p_bottom = 508 + j
-				l_img1 = l_img.crop((p_left, p_top, p_right, p_bottom))
+				left = (width/2) - 550 + (288 * x)
+				top = 485 + y
+				right = (width/2) - 310 + (288 * x)
+				bottom = 508 + y
+				p_name = l_img.crop((left, top, right, bottom))
 				
 				#CLEANING IMAGE			
-				l_img1.save('l_img'+str(count1)+'.png', dpi=(300,300))
-				l_img1 = cv2.imread('l_img'+str(count1)+'.png')
+				p_name.save('./screenshots/player'+str(count1)+'.png', dpi=(300,300))
+				p_name = cv2.imread('./screenshots/player'+str(count1)+'.png')
 				count1 += 1
-				l_img1 = cv2.resize(l_img1, None, fx=5, fy=5, interpolation=cv2.INTER_CUBIC)
-				retval, threshold = cv2.threshold(l_img1,127,255,cv2.THRESH_BINARY)
+				p_name = cv2.resize(p_name, None, fx=5, fy=5, interpolation=cv2.INTER_CUBIC)
+				retval, threshold = cv2.threshold(p_name,127,255,cv2.THRESH_BINARY)
 
 				#IMAGE TO STRING
-				result1 = pytesseract.image_to_string(threshold)
-				result1 = result1.replace("\n", "")
-				players.append(result1)
+				res = pytesseract.image_to_string(threshold)
+				res = res.replace("\n", "")
+				players.append(res)
 
 		playersLoaded = False
+
+		currMax = 0
+		currPlayer = ""
+		for player in players:
+			prob = SequenceMatcher(None, args[1], player).ratio()
+			if prob > currMax:
+				currPlayer = player
+				currMax = prob
+
+		players.remove(currPlayer)	
+
+		canPlay = players.copy()
+		cantPlay = []
 		print(players)
 
 	#EACH ROUND
 	if keyboard.is_pressed(keyBind):
-		# img = pyautogui.screenshot('test' + str(count) + '.png')
-		img = Image.open('test' + str(count) + '.png')
+		# img = pyautogui.screenshot('round' + str(count) + '.png')
+		img = Image.open('./screenshots/round' + str(count) + '.png')
 
 		#ROUND NUMBER
 		width, height = img.size
@@ -58,38 +79,39 @@ while game:
 		right =(width/2) - 150
 		top = 10
 		bottom = 35
-		round_image = img.crop((left, top, right, bottom))
-		round_image.save('round' + str(count) + '.png', dpi=(300, 300))
+		round_num_img = img.crop((left, top, right, bottom))
+		round_num_img.save('./screenshots/round_num' + str(count) + '.png', dpi=(300, 300))
 
 		#CLEANING IMAGE
-		round_image2 = cv2.imread('round'+str(count)+'.png')
-		round_image2 = cv2.resize(round_image2, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
-		retval, threshold = cv2.threshold(round_image2,127,255,cv2.THRESH_BINARY)
+		round_num_img = cv2.imread('./screenshots/round_num'+str(count)+'.png')
+		round_num_img = cv2.resize(round_num_img, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
+		retval, threshold = cv2.threshold(round_num_img,127,255,cv2.THRESH_BINARY)
 
 		#IMAGE TO STRING
-		round_result = pytesseract.image_to_string(threshold)
-		####ROUND_RESULT IS THE ROUND NUMBER
-		round_result = round_result.replace("\n", "")
+		round_num = pytesseract.image_to_string(threshold)
+		####ROUND_NUM IS THE ROUND NUMBER
+		round_num = round_num.replace("\n", "")
 
 
 		#CROPPING IMAGE
-		left = (width/2) - 118 + (int(round_result[-1]) * 34)
-		if int(round_result[-1]) == 5:
+		left = (width/2) - 118 + (int(round_num[-1]) * 34)
+		if int(round_num[-1]) == 5:
 			left = (width/2) - 118 + (4 * 34)
 
 		top = 55
 		right = left + 250
 		bottom = 87
-		img1 = img.crop((left, top, right, bottom))
+		
+		prev_player = img.crop((left, top, right, bottom))
 
 		#CLEANING IMAGE
-		img1.save('test600' + str(count) + '.png', dpi=(600,600))
-		img2 = cv2.imread('test600'+str(count)+'.png')
+		prev_player.save('./screenshots/prev_player' + str(count) + '.png', dpi=(600,600))
+		prev_player = cv2.imread('./screenshots/prev_player'+str(count)+'.png')
 
 		# img2 = Image.open('test600' +str(count) + '.png')
 		count += 1
-		img2 = cv2.resize(img2, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
-		retval, threshold = cv2.threshold(img2,127,255,cv2.THRESH_BINARY)
+		prev_player = cv2.resize(prev_player, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
+		retval, threshold = cv2.threshold(prev_player,127,255,cv2.THRESH_BINARY)
  		
  		#IMAGE TO STRING
 		result = pytesseract.image_to_string(threshold)
@@ -99,7 +121,6 @@ while game:
 		else:
 			result = result[13:]
 
-		print(result)
 		currMax = 0
 		currPlayer = ""
 		for player in players:
@@ -109,6 +130,13 @@ while game:
 				currMax = prob
 
 		print(currPlayer)
+		cantPlay.append(currPlayer)
+		canPlay.remove(currPlayer)
+		if len(cantPlay) > 4:
+			canPlay.append(cantPlay.pop(0))
+
+		print(canPlay)
+		#CURRPLAYER IS WHO YOU JUST PLAYED
 		
 
 
